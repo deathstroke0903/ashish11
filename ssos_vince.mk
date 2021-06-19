ac9eb8f14b88e4400388c4ed5e87ac682965a9e9
unknown <aksanshuashish@gmail.com>
2021-06-19 14:30:03 +0530

    changing boot animation to 1080p

--- ssos_vince.mk
+++ ssos_vince.mk
@@ -29,7 +29,7 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common ssos stuff.
 $(call inherit-product, vendor/ssos/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_DISABLE_POSTRENDER_CLEANUP := true
 
 # Device identifier. This must come after all inclusions
