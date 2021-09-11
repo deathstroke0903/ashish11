7c86def61bca70172201725d6348ea3ab0e732b7
baibhab34 <baibhab34ausers.noreply.github.com>
2021-09-11 12:43:25 +0530

    vince: Enable zram-writeback job

--- prop.mk
+++ prop.mk
@@ -321,3 +321,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.dha_th_rate=2.3 \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
+
+# Zram-writeback
+ro.zram.mark_idle_delay_mins=60 
+ro.zram.first_wb_delay_mins=180
+ro.zram.periodic_wb_delay_hours=24
